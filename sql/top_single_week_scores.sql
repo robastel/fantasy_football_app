f3bbb63a229fe10ea80bc34044df070fd3461510
Robert Astel <rob.astel@gmail.com>
2023-08-23 10:26:40 -0400

    Get matchups per season from completed seasons only

--- sql/top_single_week_scores.sql
+++ sql/top_single_week_scores.sql
@@ -15,17 +15,22 @@ WITH top_single_weeks AS
 
 , matchups_per_season AS (
     SELECT
-        1.0 * COUNT(matchup_id) / COUNT(DISTINCT season_id) AS avg_matchups_per_season
+        1.0 * COUNT(m.matchup_id) / COUNT(DISTINCT m.season_id) AS avg_matchups_per_season
     FROM
-        robboli-broc.fantasy_football.matchups
+        robboli-broc.fantasy_football.matchups AS m
+    INNER JOIN
+        robboli-broc.staging.seasons AS s
+        ON m.season_id = s.season_id
+        AND s.last_completed_week = s.total_weeks
     WHERE
-        is_median_matchup = 0
+        m.is_completed = 1
+        AND m.is_median_matchup = 0
 )
 
 SELECT
     tsw.ranking
     , tsw.manager_initials
-    , CONCAT(CAST(tsw.year AS STRING), 'W', CAST(tsw.week AS STRING)) AS week
+    , CONCAT(CAST(tsw.year AS STRING), ' Week ', CAST(tsw.week AS STRING)) AS week
     , tsw.points
     , mps.avg_matchups_per_season
 FROM
